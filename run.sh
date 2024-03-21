95db9720a32851bc95b59304e728bb63d41e07a4
San <ikhsana12a@gmail.com>
2024-03-21 20:43:32 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-0ccdcdd4-d1da-4041-9790-f0b5e0549b57/)
 
 
 echo "$curl_output" >> Update.tsx
